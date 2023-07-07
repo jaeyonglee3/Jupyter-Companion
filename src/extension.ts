d473711bb5745f0ff246ade1430d45a0cb60d970
Jaeyong Lee <lee.j1533@gmail.com>
2023-07-06 21:20:45 -0400

    add the cheer up puppy feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+const path = require('path');
+const fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,6 +15,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	// a disposable object is used to manage resources that need to be cleaned up or disposed of when they are no longer needed.
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -25,7 +28,32 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello Mars!');
 	});
 
+	let disposable3 = vscode.commands.registerCommand('helloworld.showPuppy', () => {
+		// Create a webview panel
+		const panel = vscode.window.createWebviewPanel(
+			'puppyPanel',
+			'Puppy Picture',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+	
+		// Load the puppy.html file into the webview panel
+		panel.webview.html = getWebviewContent();
+	});
+	
+	function getWebviewContent() {
+		// Read the contents of the puppy.html file and return it as a string
+		const filePath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'puppy.html'));
+		const fileContent = fs.readFileSync(filePath.fsPath, 'utf-8');
+		return fileContent;
+	}
+	
+	// Add the disposable to the context subscriptions
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable2);
+	context.subscriptions.push(disposable3);
 
 }
 
