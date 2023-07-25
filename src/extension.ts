cac85d82666e0a9297d9a960f6310d73a2f7b7d7
Jaeyong Lee <lee.j1533@gmail.com>
2023-07-25 13:17:26 -0400

    Created new cell feature, cleaned up code

--- src/extension.ts
+++ src/extension.ts
@@ -1,35 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 const path = require('path');
 const fs = require('fs');
+const { Configuration, OpenAIApi } = require("openai");
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env' });
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// a disposable object is used to manage resources that need to be cleaned up or disposed of when they are no longer needed.
 	let disposableHelloWorld = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello VS Code');
 	});
-
+	// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	let disposableHelloMars = vscode.commands.registerCommand('helloworld.hellomars', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello Mars!');
 	});
-
+	// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	let disposablePuppy = vscode.commands.registerCommand('helloworld.showPuppy', () => {
-		// Create a webview panel
 		const panel = vscode.window.createWebviewPanel(
 			'puppyPanel',
 			'Puppy Picture',
@@ -38,26 +27,88 @@ export function activate(context: vscode.ExtensionContext) {
 				enableScripts: true
 			}
 		);
-	
-		// Load the puppy.html file into the webview panel
 		panel.webview.html = getWebviewContent();
 	});
 	
-	function getWebviewContent() {
-		// Read the contents of the puppy.html file and return it as a string
+	const getWebviewContent = () => {
 		const filePath = vscode.Uri.file(path.join(context.extensionPath, 'src', 'puppy.html'));
 		const fileContent = fs.readFileSync(filePath.fsPath, 'utf-8');
 		return fileContent;
-	}
+	};
 
-	let diagnosticCollection: vscode.DiagnosticCollection;
+	// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	const askGpt = async (prompt: string) => {
+		const configuration = new Configuration({
+			apiKey: process.env.OPENAI_API_KEY,
+		});
+		const openai = new OpenAIApi(configuration);
+	  
+		const chatCompletion = await openai.createChatCompletion({
+		  model: "gpt-3.5-turbo",
+		  messages: [{ role: "user", content: prompt }],
+		});
+		console.log(chatCompletion.data.choices[0].message.content);
+		const message = chatCompletion.data.choices[0].message.content;
 
+		return message;
+	};
+
+	const createNotebook = async () => {
+		const activeEditor = vscode.window.activeTextEditor;
+		if (!activeEditor || activeEditor.document.languageId !== 'python') {
+		  vscode.window.showErrorMessage('No .ipynb file is open');
+		  return;
+		}
+		const filePath = activeEditor.document.uri.fsPath;
+	  
+		// Prompt the user to enter the prompt for the new cell
+		const topic = await vscode.window.showInputBox({
+		  placeHolder: 'Enter the topic',
+		  prompt: 'Please enter the topic you want the cell to be filled with',
+		});
+	  
+		if (!topic) {
+		  return;
+		}
+	  
+		askGpt(topic)
+		  .then(async (result) => {
+			const cell = {
+			  cell_type: 'markdown',
+			  source: ["###" + result],
+			  metadata: {},
+			  outputs: [],
+			  execution_count: null,
+			};
+	  
+			// Read the existing notebook content
+			const existingContent = await vscode.workspace.fs.readFile(vscode.Uri.file(filePath));
+			const existingNotebook = JSON.parse(existingContent.toString());
+	  
+			// Add the new cell to the existing notebook cells
+			existingNotebook.cells.push(cell);
+	  
+			// Save the updated notebook file
+			const updatedContent = JSON.stringify(existingNotebook, null, 2);
+			await vscode.workspace.fs.writeFile(vscode.Uri.file(filePath), Buffer.from(updatedContent));
+	  
+			// Refresh the editor to reflect the changes
+			activeEditor.document.save();
+			vscode.commands.executeCommand('workbench.action.files.revert');
+	  
+			// Show success message to the user
+			vscode.window.showInformationMessage('New cell added to the notebook');
+		  })
+		  .catch((error) => {
+			console.error(error);
+		  });
+	  };	  
+
+	// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// Add the disposable to the context subscriptions
 	context.subscriptions.push(disposableHelloWorld);
 	context.subscriptions.push(disposableHelloMars);
 	context.subscriptions.push(disposablePuppy);
-
+	context.subscriptions.push(vscode.commands.registerCommand('helloworld.createNotebook', createNotebook));
 }
-
-// This method is called when your extension is deactivated
 export function deactivate() {}
